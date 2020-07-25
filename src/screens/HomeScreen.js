4106d8e304588782a02da6a7b3e20bb5069a4dd3
Chris Tack <chris_tack@mac.com>
2020-07-24 22:56:08 -0700

    Refactor to use destructuring

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = (props) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello World!</Text>
       <Button
-        onPress={() => props.navigation.navigate('Components')}
+        onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo"
       />
       <Button
-        onPress={() => props.navigation.navigate('List')}
+        onPress={() => navigation.navigate('List')}
         title="Go to List"
       />
     </View>
