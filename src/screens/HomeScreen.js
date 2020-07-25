9935b99242899dce1d2ec9b3a9db34d6c94326ca
Chris Tack <chris_tack@mac.com>
2020-07-24 22:51:05 -0700

    Pass props and add navigation to button

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.text}>Hello World!</Text>
       <Button
-        onPress={() => console.log('Button Pressed')}
+        onPress={() => props.navigation.navigate('Components')}
         title="Go to Components Demo"
       />
       <TouchableOpacity onPress={() => console.log('List Pressed')}>
