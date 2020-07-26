11f2a4b8cfa42d983d21b5169fc1ebb85c3868db
Chris Tack <chris_tack@mac.com>
2020-07-25 22:27:44 -0700

    Import and use props

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ImageDetail = () => {
-  return <Text>Image Detail</Text>;
+const ImageDetail = (props) => {
+return <Text>{props.title}</Text>;
 };
 
 const styles = StyleSheet.create({});
