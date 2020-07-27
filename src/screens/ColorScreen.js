425eac4b4a36b8c27eb95e5219d80a63343279e3
Chris Tack <chris_tack@mac.com>
2020-07-27 14:28:54 -0700

    Import FlatList

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, FlatList } from 'react-native';
 
 const ColorScreen = () => {
   const [colors, setColors] = useState([])
