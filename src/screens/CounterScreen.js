2715f3714b122dbd1d9b1186350465a5a7f09656
Chris Tack <chris_tack@mac.com>
2020-07-26 22:32:56 -0700

    Use useState hook to modify counter

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () =>{
+  const [counter, setCounter] = useState(0);
+  
   return (
     <View>
+      <Button title="Increase" onPress={() => setCounter(counter + 1)} />
+      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
       <Text>Current Count: </Text>
     </View>
   );
