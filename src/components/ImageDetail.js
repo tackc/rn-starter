142b2f735af58ff0511c1b8159bccea56418a81c
Chris Tack <chris_tack@mac.com>
2020-07-26 21:39:32 -0700

    Update return statement

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = ({ imageSource, title, score }) => {
-return <View>
-    <Image source={imageSource} />
-    <Text>{title}</Text>
-    <Text>Image score - {score}</Text>
-  </View>
+return (
+  <View>
+      <Image source={imageSource} />
+      <Text>{title}</Text>
+      <Text>Image score - {score}</Text>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({});
