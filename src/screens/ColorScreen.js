fe73e210e4b846c902f804d4162527fa8946a83d
Chris Tack <chris_tack@mac.com>
2020-07-27 14:18:16 -0700

    Create initial color box

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -5,6 +5,7 @@ const ColorScreen = () => {
   return (
     <View>
       <Button title="Add a Color" />
+      <View style={{ height: 100, width: 100, backgroundColor: 'rgb(0, 255, 0)' }} />
     </View>
   );
 };
