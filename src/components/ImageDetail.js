15374bb595741d79a3c3853927813280dc2ffc1e
Chris Tack <chris_tack@mac.com>
2020-07-26 21:27:37 -0700

    Import and use Image

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = (props) => {
-return <Text>{props.title}</Text>;
+return <View>
+    <Image source={props.imageSource} />
+    <Text>{props.title}</Text>
+  </View>
 };
 
 const styles = StyleSheet.create({});
