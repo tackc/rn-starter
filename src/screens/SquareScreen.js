3d0502b258557faa35cf7b5e618bd17d04f6d7c3
Chris Tack <chris_tack@mac.com>
2020-07-28 21:09:08 -0700

    Create SquareScreen

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SquareScreen = () => {
+  return (
+    <Text>Square Screen</Text>
+  );
+};
+
+export default SquareScreen;
