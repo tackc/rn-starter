4d466448534d441667c1922e586d48bd7afe9011
Chris Tack <chris_tack@mac.com>
2020-07-29 21:31:21 -0700

    Create increment const

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
 const [red, setRed] = useState(0);
 const [green, setGreen] = useState(0);
