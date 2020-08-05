2bf93f201103c1667c01887501da5f484d5a5049
Chris Tack <chris_tack@mac.com>
2020-08-04 22:19:59 -0700

    Add button for box screen demo

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -33,6 +33,10 @@ const HomeScreen = ({ navigation }) => {
         onPress={() => navigation.navigate('Text')}
         title="Go to Text Screen Demo"
       />
+      <Button 
+        onPress={() => navigation.navigate('Box')}
+        title="Go to Box Screen Demo"
+      />
     </View>
     );
 };
